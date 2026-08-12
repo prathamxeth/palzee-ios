acb37cd3700e39b7a9615d9bb4963d78d2fbdde4
PRATHAM <prathamup123@gmail.com>
2026-08-12 16:57:54 +0530

    refactor: remove redundant android audio settings, clean up unused assets, and update xcode build dependencies

--- components/camera/PalVideoSendPreviewModal.tsx
+++ components/camera/PalVideoSendPreviewModal.tsx
@@ -177,7 +177,6 @@ export default function PalVideoSendPreviewModal({
       allowsRecordingIOS: false,
       playsInSilentModeIOS: true,
       staysActiveInBackground: false,
-      shouldDuckAndroid: true,
     }).catch(() => {});
   }, []);
 
